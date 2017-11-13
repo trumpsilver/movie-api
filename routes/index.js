06ec82693b79973b791f1be377d5e9d3f5e21aa7
nvuhung <nvuhung@outlook.com>
2017-11-13 07:33:04 +0700

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../public/javascripts/controller');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
+});
+
+router.get('/get-list-cinemas', controller.getListCinemas);
+router.get('/get-cinema-detail', controller.getCinemaDetail);
+router.get('/get-list-showing-films', controller.getListShowingFilms);
+router.get('/get-film-detail', controller.getFilmDetail);
+router.get('/get-session', controller.getSession);
+
+module.exports = router;
